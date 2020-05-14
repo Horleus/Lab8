cef951c7a8c9f7d0470f25d9599b6cf8ca188793
tim <tim@TIMSPC>
2020-05-14 18:06:45 +0300

    Впорядковано вивід

--- src/Lab8.cpp
+++ src/Lab8.cpp
@@ -4,24 +4,21 @@ using namespace std;
 
 int main() {
 	double tgf,k1,k2,f; // Ідентифікація зміних для формул
+	double PI = 3.1415926;
 
-	cout << "Enter value of k1:";
+	cout << "Пряма y=k1x+b1, введіть k1:";
     cin >> k1;
     cout << endl;
 
-    cout << "Enter value of k2:";
+    cout << "Пряма y=k2x+b2, введіть k2:";
     cin >> k2;
     cout << endl;
 
-    tgf=(k2-k1)/(1+k1*k2);
+    tgf=abs((k2-k1)/(1+k1*k2));
 
-    f=atan(tgf);
+    f=atan(tgf)*180/PI;
 
-    cout << f;
-    cout << endl;
-
+    cout << "Кут між прямими: " << f << "градусів" << endl;
 
     return 0;
-
-
 }
