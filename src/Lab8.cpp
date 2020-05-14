2c2372fc08268a8cdfe3cc46ae948687c4951fcb
f5 <f5@f5-X51RL>
2020-05-14 16:43:52 +0300

    Без формули тгф

--- src/Lab8.cpp
+++ src/Lab8.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int main() {
-	double tgf,k1,k2,y1,y2,x,b1,b2,f; // Ідентифікація зміних для формул
-	tgf=k1-k2/1+k1*k2; // Формула обчислення tgf
-    k1=2; // Значення к1
-    k2=4; // Значення к2
-    x=2; // Значення х
-    b1=6; // Значення b1
-    b2=8; // Значення b2
-    y1=k1*x+b1; // Значення прямої y1
-    y2=k2*x+b2; // Значення прямої y2
-    f=
+	double tgf,k1,k2,f; // Ідентифікація зміних для формул
+
+	cout << "Enter value of k1:";
+    cin >> k1;
+    cout << endl;
+
+    cout << "Enter value of k2:";
+    cin >> k2;
+    cout << endl;
+
+    cout << "tgf=" <<  << endl;
+    return 0;
 
 
 }
