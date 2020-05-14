58b79208cbdfaf38c7d9efa66d1fdf4cab122895
tim <tim@TIMSPC>
2020-05-14 12:59:43 +0300

    Added entering of values

--- src/Lab1.cpp
+++ src/Lab1.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+	double a;
+	cout << "Enter value of a:";
+	cin >> a;
+	cout << endl;
+
+	cout << "Value of a is " << a << endl;
+
 	cout << "Hello World!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
